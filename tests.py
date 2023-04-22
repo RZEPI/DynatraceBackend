e7eaf6373c46121331aa7c61257527b54a2743dd
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2023-04-22 18:35:25 +0200

    test added

--- tests.py
+++ tests.py
@@ -16,12 +16,50 @@ class TestAvgExch(unittest.TestCase):
     def test_invalid_curr_code(self):
         response = self.app.get('/exchanges/21/2023-01-02')
         response_data = json.loads(response.text)
-        # print(response)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_data['result'], "Invalid currency code format")
+        self.assertEqual(response_data['result'],
+                         "Invalid currency code format")
 
     def test_invalid_date(self):
         response = self.app.get('/exchanges/GBP/sdasa')
         response_data = json.loads(response.text)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response_data['result'], "Invalid date format")
+
+class TestMinMaxVal(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_valid_input(self):
+        response = self.app.get('/minmaxval/eur/2')
+        response_data = json.loads(response.text)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data['result'], [4.6039, 4.6109])
+
+    def test_invalid_curr_code(self):
+        response = self.app.get('/minmaxval/21/2')
+        response_data = json.loads(response.text)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data['result'],
+                         "Invalid currency code format")
+    
+    def test_invalid_quot_num_zero(self):
+        response = self.app.get('/minmaxval/eur/0')
+        response_data = json.loads(response.text)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data['result'],
+                         "Invalid number of quotations format")
+    
+    def test_invalid_quot_num_too_high(self):
+        response = self.app.get('/minmaxval/eur/300')
+        response_data = json.loads(response.text)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data['result'],
+                         "Invalid number of quotations format")
+        
+    def test_invalid_quot_num_nan(self):
+        response = self.app.get('/minmaxval/eur/hi')
+        response_data = json.loads(response.text)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data['result'],
+                         "Invalid number of quotations format")
